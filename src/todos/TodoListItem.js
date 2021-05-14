df5ba82b3e132caed4dc0c7d29a5a8397c5c24c3
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-05-14 16:04:27 +0300

    add adding/deleting todoList items configure redux-persist

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './TodoListItem.css'
 
-const TodoListItem = ({ todo }) => {
+const TodoListItem = ({ todo, onRemovePressed }) => {
     return (
         <div className="todo-item-container">
             <h1>{todo.text}</h1>
             <div className="button-container">
                 <button className="completed-button">Mark As Completed</button>
-                <button className="remove-button">Remove</button>
+                <button
+                    onClick={() => {
+                        onRemovePressed(todo.text)
+                    }}
+                    className="remove-button">
+                    Remove
+                </button>
             </div>
         </div>
     )
